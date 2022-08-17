2d5d92eae265724c9c78358a068f65340adf2867
khunzinshine <khunzinshine@ayainnovation.com>
2022-08-17 20:42:01 +0630

    implement basic custom react hooks

---
+++ src/components/BackwardCounter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './Card';
+import useCounter from '../hooks/useCounter';
+
+const BackwardCounter = () => {
+  //set forwards to false
+  const counter = useCounter(false);
+  
+  return <Card>{counter}</Card>;
+};
+
+export default BackwardCounter;
